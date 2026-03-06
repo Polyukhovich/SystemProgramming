9a8414ec3358ead70cd056f3f401979b9b34b776
Polyukhovich Andriy <144113422+Polyukhovich@users.noreply.github.com>
2026-03-06 13:17:16 +0200

    Add files via upload

---
+++ lab1/Worker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab1_Task2
+{
+    public static class Worker
+    {
+        // Метод для пріоритетних (звичайних) потоків
+        public static void NormalThreadTask()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} is executing (step {i})");
+                Thread.Sleep(500); // Пауза півсекунди
+            }
+            Console.WriteLine($"--- {Thread.CurrentThread.Name} FINISHED ---");
+        }
+
+        // Метод для фонового потоку (нескінченний цикл)
+        public static void BackgroundThreadTask()
+        {
+            int count = 1;
+            while (true)
+            {
+                Console.WriteLine($"[BG] Background thread is working... ({count++})");
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
