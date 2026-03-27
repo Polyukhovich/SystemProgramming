d20ff34725d6f8a2e630cc82a8e0528676cb4f61
Polyukhovich Andriy <144113422+Polyukhovich@users.noreply.github.com>
2026-03-27 13:26:30 +0200

    Add files via upload

---
+++ lab4/Lab/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Lab
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonStart = new Button();
+            buttonCancel = new Button();
+            progressBar1 = new ProgressBar();
+            labelProgress = new Label();
+            labelResult = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            buttonStart.AccessibleName = "";
+            buttonStart.Location = new Point(78, 380);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(75, 23);
+            buttonStart.TabIndex = 0;
+            buttonStart.Text = "Start";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // buttonCancel
+            // 
+            buttonCancel.Location = new Point(623, 380);
+            buttonCancel.Name = "buttonCancel";
+            buttonCancel.Size = new Size(75, 23);
+            buttonCancel.TabIndex = 1;
+            buttonCancel.Text = "Cancel";
+            buttonCancel.UseVisualStyleBackColor = true;
+            buttonCancel.Click += buttonCancel_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(160, 196);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(458, 23);
+            progressBar1.TabIndex = 2;
+            progressBar1.Click += progressBar1_Click;
+            // 
+            // labelProgress
+            // 
+            labelProgress.AutoSize = true;
+            labelProgress.Location = new Point(392, 178);
+            labelProgress.Name = "labelProgress";
+            labelProgress.Size = new Size(23, 15);
+            labelProgress.TabIndex = 3;
+            labelProgress.Text = "0%";
+            // 
+            // labelResult
+            // 
+            labelResult.AutoSize = true;
+            labelResult.Location = new Point(160, 239);
+            labelResult.Name = "labelResult";
+            labelResult.Size = new Size(42, 15);
+            labelResult.TabIndex = 4;
+            labelResult.Text = "Result:";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(78, 115);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(94, 19);
+            radioButton1.TabIndex = 5;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(357, 115);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(94, 19);
+            radioButton2.TabIndex = 6;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "radioButton2";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(623, 115);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(94, 19);
+            radioButton3.TabIndex = 7;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "radioButton3";
+            radioButton3.UseVisualStyleBackColor = true;
+            radioButton3.CheckedChanged += radioButton3_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(labelResult);
+            Controls.Add(labelProgress);
+            Controls.Add(progressBar1);
+            Controls.Add(buttonCancel);
+            Controls.Add(buttonStart);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonStart;
+        private Button buttonCancel;
+        private ProgressBar progressBar1;
+        private Label labelProgress;
+        private Label labelResult;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+    }
+}
---
+++ lab4/Lab/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Lab
+{
+    public partial class Form1 : Form
+    {
+        // [ЗАВДАННЯ 4]: Об'єкт для керування скасуванням асинхронної задачі
+        private CancellationTokenSource cts;
+
+        // Змінна, яка буде контролювати швидкість роботи (затримку в мілісекундах)
+        private int delayTime = 50;
+
+        // Змінна кольору прогрес бару 
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr w, IntPtr l);
+        const int PBM_SETSTATE = 1040;
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Text = "Advanced Async Calculator";
+            this.BackColor = Color.WhiteSmoke;
+
+            // [ЗАВДАННЯ 1]: Радіокнопки потрібні для демонстрації того, що головний потік 
+            radioButton1.Text = "Fast (10 ms)";
+            radioButton2.Text = "Normal (50 ms)";
+            radioButton3.Text = "Slow (200 ms)";
+            radioButton2.Checked = true;
+
+            buttonCancel.Enabled = false;
+        }
+        // [ЗАВДАННЯ 1]: Запуск асинхронної задачі. Метод позначено як 'async', 
+        private async void buttonStart_Click(object sender, EventArgs e)
+        {
+            buttonStart.Enabled = false;
+
+            buttonCancel.Enabled = true;
+
+            SendMessage(progressBar1.Handle, PBM_SETSTATE, (IntPtr)1, IntPtr.Zero);
+
+            labelResult.Text = "Result: calculating...";
+            labelResult.ForeColor = Color.Black;
+            progressBar1.Value = 0;
+
+            // [ЗАВДАННЯ 3]: Інформування про хід виконання. 
+            // Створюємо об'єкт IProgress, який оновлює відсотки у тексті та сам прогрес-бар.
+            IProgress<int> onChangeProgress = new Progress<int>((i) =>
+            {
+                labelProgress.Text = i.ToString() + "%";
+                progressBar1.Value = i;
+            });
+
+            // [ЗАВДАННЯ 4]: Ініціалізація токена скасування перед початком роботи.
+            cts = new CancellationTokenSource();
+
+            try
+            {
+                // [ЗАВДАННЯ 1] та [ЗАВДАННЯ 2]: 
+                // Виклик довготривалої операції через 'await' (не блокує форму).
+                // Отримання результату обчислень (result) після завершення задачі.
+                int result = await ProcessAsync(100, onChangeProgress, cts.Token);
+
+                // [ЗАВДАННЯ 2]: Відображення отриманого результату на елементі керування форми.
+                if (!cts.IsCancellationRequested)
+                {
+                    labelResult.Text = "Result: " + result.ToString();
+                    labelResult.ForeColor = Color.DarkGreen;
+                }
+            }
+            finally
+            {
+                buttonStart.Enabled = true;
+                buttonCancel.Enabled = false;
+            }
+        }
+
+        // [ЗАВДАННЯ 4]: Дочасне переривання довготривалої операції
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            if (cts != null && !cts.IsCancellationRequested)
+            {
+                // Переводимо токен у стан "скасований"
+                cts.Cancel();
+
+                SendMessage(progressBar1.Handle, PBM_SETSTATE, (IntPtr)2, IntPtr.Zero);
+
+                labelResult.Text = "Result: CANCELED ";
+                labelResult.ForeColor = Color.Red;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked) delayTime = 10;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked) delayTime = 50;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked) delayTime = 200;
+        }
+
+        // асинхрон        // Метод повертає Task<int>
+        private Task<int> ProcessAsync(int count, IProgress<int> changeProgressBar, CancellationToken cancellationToken)
+        {
+            // [ЗАВДАННЯ 1]: Запускаємо код в окремому фоновому потоці за допомогою Task.Run
+            return Task.Run(() =>
+            {
+                int sum = 0;
+
+                for (int i = 1; i <= count; i++)
+                {
+                    // [ЗАВДАННЯ 4]: Перевірка того, чи має бути скасована операція на кожній ітерації
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        return sum; // Перериваємо цикл і повертаємо те, що встигли порахувати
+                    }
+
+                    sum += i; // Імітація обчислень (Завдання 2)
+
+                    // [ЗАВДАННЯ 3]: Передаємо поточний відсоток (i) назад на головну форму
+                    changeProgressBar.Report(i);
+
+                    // Штучна затримка, що імітує "довготривалу операцію" 
+                    Thread.Sleep(delayTime);
+                }
+
+                // [ЗАВДАННЯ 2]: Повертаємо фінальний результат
+                return sum;
+            });
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
